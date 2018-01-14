093240f673d8f3542f1a29d0635c09a6769af704
cs <cs.temporary@icloud.com>
2018-01-14 11:37:08 -0800

    Change date and time entry in createEvent to use picker fragments - Selection of date now through pop-up calendar - Selection of time now through pop-up clock - Add buttons to set date and time in content_create_event.xml - Selected date and time displayed next to buttons   - Text displays specified in content_create_event.xml - Selection and display logic in CreateEventActivity.java - Add strings for button text to strings.xml

--- app/src/main/java/org/codethechange/culturemesh/CreateEventActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/CreateEventActivity.java
@@ -1,11 +1,19 @@
 package org.codethechange.culturemesh;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -17,6 +25,66 @@ public class CreateEventActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // TODO: Locale adjustments for time (e.g. AM/PM vs 24-hr)
+            String time = hourOfDay + ":" + minute;
+            // SOURCE: https://stackoverflow.com/questions/26917564/set-textview-to-time-from-timepicker-android?rq=1
+            TextView textView = getActivity().findViewById(R.id.eventTime);
+            textView.setText(time);
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // TODO: Locale adjustments for date
+            month ++;   // It seems months start at 0
+            String date = month + "/" + day + "/" + year;
+            // SOURCE: https://stackoverflow.com/questions/26917564/set-textview-to-time-from-timepicker-android?rq=1
+            TextView textView = getActivity().findViewById(R.id.eventDate);
+            textView.setText(date);
+        }
     }
 
 }
