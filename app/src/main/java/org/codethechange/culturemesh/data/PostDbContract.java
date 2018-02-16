ff3364d366b88c624ae02fa2b8b61ffee0727ca9
DrewGregory <djgregny@gmail.com>
2018-02-16 13:05:30 -0800

    Schema for posts

--- app/src/main/java/org/codethechange/culturemesh/data/PostDbContract.java
+++ app/src/main/java/org/codethechange/culturemesh/data/PostDbContract.java
@@ -1,9 +1,25 @@
 package org.codethechange.culturemesh.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Drew Gregory on 2/16/18.
  */
 
 public class PostDbContract {
-    //TODO: Write SCHEMA
+    private PostDbContract() {
+
+    }
+    public static class PostDbEntry implements BaseColumns {
+        public static final String TABLE_NAME = "posts";
+        //The location info are all id's.
+        public static final String COLUMN_NAME_USER_ID = "user_id";
+        public static final String COLUMN_NAME_NETWORK_ID = "network_id";
+        public static final String COLUMN_NAME_POST_DATE = "post_date";
+        public static final String COLUMN_NAME_TEXT = "post_text";
+        public static final String COLUMN_NAME_POST_CLASS = "post_class";
+        public static final String COLUMN_NAME_POST_ORIGINAL = "post_original";
+        public static final String COLUMN_NAME_VID_LINK = "vid_link";
+        public static final String COLUMN_NAME_IMG_LINK = "img_link";
+    }
 }
--- app/src/main/java/org/codethechange/culturemesh/data/PostDbHelper.java
+++ app/src/main/java/org/codethechange/culturemesh/data/PostDbHelper.java
@@ -18,11 +18,22 @@ public class PostDbHelper extends SQLiteOpenHelper  {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //TODO: write SQL CMD
+        final String CREATE_TABLE = "CREATE TABLE " + PostDbContract.PostDbEntry.TABLE_NAME + " (" +
+                PostDbContract.PostDbEntry._ID + " INTEGER PRIMARY KEY, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_IMG_LINK + " TEXT, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_NETWORK_ID + " INTEGER, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_POST_CLASS + " INTEGER, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_POST_DATE + " TIMESTAMP, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_POST_ORIGINAL + " TEXT, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_TEXT + " TEXT, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_USER_ID + " INTEGER, " +
+                PostDbContract.PostDbEntry.COLUMN_NAME_VID_LINK + " TEXT);";
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        onCreate(db);
     }
 }
