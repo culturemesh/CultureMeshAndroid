0f83e772e15deae18c338812631f08aeac122f31
cs <cs.temporary@icloud.com>
2018-03-02 21:21:42 +0000

    Document PickOnboardingStatusActivity

--- app/src/main/java/org/codethechange/culturemesh/PickOnboardingStatusActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/PickOnboardingStatusActivity.java
@@ -2,14 +2,16 @@ package org.codethechange.culturemesh;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class PickOnboardingStatusActivity extends AppCompatActivity {
 
+    /**
+     * When Activity created, display the layout file
+     * @param savedInstanceState Saved state to restore
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +20,28 @@ public class PickOnboardingStatusActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    /**
+     * If button for a returning user tapped, send user to their Timeline
+     * @param v View the user tapped the button on
+     */
     public void tapReturning(View v) {
         Intent start = new Intent(getApplicationContext(), TimelineActivity.class);
         startActivity(start);
     }
 
+    /**
+     * If button for a new user tapped, send user to the onboarding activity
+     * @param v View the user tapped the button on
+     */
     public void tapNew(View v) {
         Intent start = new Intent(getApplicationContext(), OnboardActivity.class);
         startActivity(start);
     }
 
+    /**
+     * If button for a returning user with no networks tapped, send user to explore networks
+     * @param v View the user tapped the button on
+     */
     public void tapNoNetworks(View v) {
         Intent start = new Intent(getApplicationContext(), ExploreBubblesOpenGLActivity.class);
         startActivity(start);
