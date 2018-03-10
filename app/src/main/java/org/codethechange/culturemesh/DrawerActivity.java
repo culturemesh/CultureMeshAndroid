863f97b891d2517d7d8fb7c76071e835974684b4
cs <cs.temporary@icloud.com>
2018-03-10 02:17:12 +0000

    Remove unneeded drawer navigation conditional and accompanying warning suppression

--- app/src/main/java/org/codethechange/culturemesh/DrawerActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/DrawerActivity.java
@@ -128,7 +128,6 @@ public class DrawerActivity extends AppCompatActivity implements NavigationView.
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         //TODO: Handle navigation view item clicks here.
@@ -147,8 +146,6 @@ public class DrawerActivity extends AppCompatActivity implements NavigationView.
         } else if (id == R.id.nav_join_network) {
             Intent startFindNet = new Intent(getApplicationContext(), FindNetworkActivity.class);
             startActivity(startFindNet);
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_about) {
             Intent startAbout = new Intent(getApplicationContext(), AboutActivity.class);
             startActivity(startAbout);
