f01a1d62b56f5d60cded7a58d9ee763173df12ae
DrewGregory <djgregny@gmail.com>
2018-03-23 21:45:52 -0700

    Updated API with autocomplete.

--- app/src/main/java/org/codethechange/culturemesh/data/CMDatabase.java
+++ app/src/main/java/org/codethechange/culturemesh/data/CMDatabase.java
@@ -17,9 +17,9 @@ import org.codethechange.culturemesh.models.User;
 
 /**
  * Created by Drew Gregory on 2/19/18.
- * TODO: Check out LiveData: https://developer.android.com/reference/android/arch/lifecycle/LiveData.htmls
+ * TODO: Check out LiveData: https://developer.android.com/reference/android/arch/lifecycle/LiveData.html
  */
-@Database(version = 12, entities = {Network.class, User.class, Post.class, Event.class,
+@Database(version = 13, entities = {Network.class, User.class, Post.class, Event.class,
         EventSubscription.class, NetworkSubscription.class, Region.class, City.class, Country.class,
         Language.class, PostReply.class})
 abstract public class CMDatabase extends RoomDatabase{
