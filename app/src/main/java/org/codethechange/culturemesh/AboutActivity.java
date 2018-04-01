a07c5f0fbadeeb22c543264b4b3b008704238e7a
cs <cs.temporary@icloud.com>
2018-04-01 15:20:00 +0000

    Make AboutActivity extend DrawerActivity

--- app/src/main/java/org/codethechange/culturemesh/AboutActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/AboutActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends DrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
