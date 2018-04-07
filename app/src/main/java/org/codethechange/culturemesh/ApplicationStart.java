3ab1d9c6da54af54e70ec35636eb3390daf71a79
DrewGregory <djgregny@gmail.com>
2018-04-07 12:09:03 -0700

    Fixed About Nav stuffs.

--- app/src/main/java/org/codethechange/culturemesh/ApplicationStart.java
+++ app/src/main/java/org/codethechange/culturemesh/ApplicationStart.java
@@ -3,12 +3,15 @@ package org.codethechange.culturemesh;
 import android.app.Application;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class ApplicationStart extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         SharedPreferences settings = getSharedPreferences(API.SETTINGS_IDENTIFIER, MODE_PRIVATE);
         if (LoginActivity.isLoggedIn(settings)) {
             if (settings.contains(API.SELECTED_NETWORK)) {
