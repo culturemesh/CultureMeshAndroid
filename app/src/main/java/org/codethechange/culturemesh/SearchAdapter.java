1e64f2d5e05fe733831f19bc0d13a1fce3cea59f
cs <cs.temporary@icloud.com>
2018-04-21 00:40:39 +0000

    Make SearchAdapter Rely on Superclass Instead of storing data in subclass lists, leverage existing superclass infrastructure so that superclass methods now have effect. This lets methods like addAll() or clear() work.

--- app/src/main/java/org/codethechange/culturemesh/SearchAdapter.java
+++ app/src/main/java/org/codethechange/culturemesh/SearchAdapter.java
@@ -12,13 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class SearchAdapter<T extends Searchable & Listable> extends ArrayAdapter<T> implements Filterable {
-    private ArrayList<T> items;
-    private ArrayList<T> filteredItems;
     private Context context;
     // R.id.location_language_name_list_view
     private int listViewID;
@@ -28,8 +24,6 @@ public class SearchAdapter<T extends Searchable & Listable> extends ArrayAdapter
                          @NonNull List<T> items) {
         super(context, resource, items);
         this.context = context;
-        this.items = new ArrayList<>(items);
-        filteredItems = new ArrayList<>(items);
         this.listViewID = listViewID;
         getFilter();
     }
@@ -44,30 +38,6 @@ public class SearchAdapter<T extends Searchable & Listable> extends ArrayAdapter
         this.listViewID = listViewID;
     }
 
-    @NonNull
-    @Override
-    public android.widget.Filter getFilter() {
-        if (filter == null) {
-            filter = new SearchAdapter.GenericFilter();
-        }
-        return filter;
-    }
-
-    @Override
-    public T getItem(int position) {
-        Log.i("Hello", filteredItems.get(position).toString());
-        return filteredItems.get(position);
-    }
-
-    /**
-     * Get size of user list
-     * @return userList size
-     */
-    @Override
-    public int getCount() {
-        return filteredItems.size();
-    }
-
 
     @NonNull
     @Override
@@ -77,36 +47,8 @@ public class SearchAdapter<T extends Searchable & Listable> extends ArrayAdapter
                     false);
         }
         TextView itemName = convertView.findViewById(listViewID);
-        itemName.setText(filteredItems.get(position).getListableName());
+        itemName.setText(getItem(position).getListableName());
         //TODO: Set number of people with .getNumUsers()
         return convertView;
-    }
-
-    private class GenericFilter extends Filter {
-
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            FilterResults filterResults = new FilterResults();
-            ArrayList<T> tempList = new ArrayList<>();
-            if (constraint != null && constraint.length() > 0) {
-                for (T item : items) {
-                    if (item.matches(constraint)) {
-                        tempList.add(item);
-                    }
-                }
-            }
-            filterResults.count = tempList.size();
-            filterResults.values = tempList;
-            return filterResults;
-        }
-
-
-        @SuppressWarnings("unchecked")
-        @Override
-        protected void publishResults(CharSequence constraint, FilterResults results) {
-            Log.i("Results" , results.values.toString());
-            filteredItems = (ArrayList<T>) results.values;
-            notifyDataSetChanged();
-        }
     }
 }
