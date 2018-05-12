40bd84da2847e72a4ac42a14fc5dbebb75ad57fe
cs <cs.temporary@icloud.com>
2018-05-12 05:14:36 +0000

    Make Country handle IsoA2 values even though they aren't used

--- app/src/main/java/org/codethechange/culturemesh/models/Country.java
+++ app/src/main/java/org/codethechange/culturemesh/models/Country.java
@@ -10,18 +10,20 @@ import org.json.JSONObject;
  */
 @Entity
 public class Country extends Place {
-    // TODO: What is isoA2?
+    // TODO: How does the isoA2 long get turned into 2-letter country codes?
     public long isoA2;
     private String name;
 
-    public Country(long id, String name, Point latLng, long population, String featureCode) {
+    public Country(long id, String name, Point latLng, long population, String featureCode, long isoA2) {
         super(id, Location.NOWHERE, Location.NOWHERE, latLng, population, featureCode);
         this.name = name;
+        this.isoA2 = isoA2;
     }
 
     public Country(JSONObject json) throws JSONException {
         super(json);
         this.name = json.getString("name");
+        this.isoA2 = json.getLong("iso_a2");
     }
 
     public String getListableName() {
