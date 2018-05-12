67fdaa412cb4b6d70d06bfa465181a973c9d1b9c
cs <cs.temporary@icloud.com>
2018-05-12 05:45:47 +0000

    Update ChooseNearLocationActivity to work with Places instead of NearLocations

--- app/src/main/java/org/codethechange/culturemesh/ChooseNearLocationActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/ChooseNearLocationActivity.java
@@ -3,32 +3,29 @@ package org.codethechange.culturemesh;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
 
-import org.codethechange.culturemesh.models.NearLocation;
+import org.codethechange.culturemesh.models.Place;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static android.view.View.GONE;
 
 public class ChooseNearLocationActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    SearchAdapter<NearLocation> adapter;
-    ListView searchList;
-    ArrayList<NearLocation> nearLocations;
-    public static final String CHOSEN_CITY = "chosen_city";
-    public static final String CHOSEN_REGION = "chosen_region";
-    public static final String CHOSEN_COUNTRY = "chosen_country";
+    public static final String CHOSEN_PLACE = "chosen_place";
     public final int RESULT_OK = 1;
+
+    private SearchAdapter<Place> adapter;
+    private ListView searchList;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +37,23 @@ public class ChooseNearLocationActivity extends AppCompatActivity implements Sea
         //Set up Search functionality.
         SearchManager mSearchManager = (SearchManager)
                 getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) findViewById(R.id.near_location_search_view);
+        searchView = findViewById(R.id.near_location_search_view);
         searchView.setOnQueryTextListener(this);
         searchView.setSearchableInfo(mSearchManager.
                 getSearchableInfo(getComponentName()));
-        searchList = (ListView) findViewById(R.id.near_location_search_results_list_view);
-        nearLocations = new ArrayList<>();
-        // TODO: Remove these dummy locations and replace with querying API when user submits (see FindNetworkActivity)
-        nearLocations.add(new NearLocation(1,1,1));
-        nearLocations.add(new NearLocation(2,2,2));
+        searchList = findViewById(R.id.near_location_search_results_list_view);
+
         adapter = new SearchAdapter<>(this, android.R.layout.simple_list_item_1,
-                R.id.location_language_name_list_view, nearLocations);
+                R.id.location_language_name_list_view);
         searchList.setTextFilterEnabled(true);
         searchList.setAdapter(adapter);
         searchList.setOnItemClickListener(new ListView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                NearLocation near = nearLocations.get(position);
+                Place near = adapter.getItem(position);
                 Intent result = new Intent();
-                result.putExtra(CHOSEN_CITY, near.near_city_id);
-                result.putExtra(CHOSEN_REGION, near.near_region_id);
-                result.putExtra(CHOSEN_COUNTRY, near.near_country_id);
+                result.putExtra(CHOSEN_PLACE, near);
                 setResult(RESULT_OK, result);
                 finish();
             }
@@ -71,14 +63,44 @@ public class ChooseNearLocationActivity extends AppCompatActivity implements Sea
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        return false;
+        search();
+        return true;
+    }
+
+    public void search() {
+        String query = searchView.getQuery().toString();
+        new searchPlaces().execute(query);
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        Toast.makeText(this, "Changing text", Toast.LENGTH_LONG).show();
-        adapter.getFilter().filter(newText);
-        //searchList.setAdapter(adapter);
         return true;
+    }
+
+    // TODO: Remove this redundant code as it is copied from FindNetworkActivity
+    // TODO: Should this AsyncTask be static?
+    class searchPlaces extends AsyncTask<String, Void, NetworkResponse<List<Place>>> {
+        @Override
+        protected NetworkResponse<List<Place>> doInBackground(String... strings) {
+            API.loadAppDatabase(getApplicationContext());
+            NetworkResponse<List<Place>> response = API.Get.autocompletePlace(strings[0]);
+            API.closeDatabase();
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(NetworkResponse<List<Place>> response) {
+            super.onPostExecute(response);
+            if (response.fail()) {
+                response.showErrorDialog(getApplicationContext());
+            } else {
+                adapter.clear();
+                adapter.addAll(response.getPayload());
+                if (searchList.getVisibility() == GONE) {
+                    searchList.setVisibility(View.VISIBLE);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 }
