7abd4414fff7606c2f9d4e678c4b3c61d2a63823
cs <cs.temporary@icloud.com>
2018-05-12 19:05:56 +0000

    Document FromLocation.java and NearLocation.java

--- app/src/main/java/org/codethechange/culturemesh/models/FromLocation.java
+++ app/src/main/java/org/codethechange/culturemesh/models/FromLocation.java
@@ -1,36 +1,64 @@
 package org.codethechange.culturemesh.models;
 
-import org.codethechange.culturemesh.Listable;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 /**
- * Created by Drew Gregory on 2/19/18.
- * Exact copy of Location, but used as Embedded Entity in SQLite Database.
+ * Wrapper for {@code DatabaseLocation} that is for From locations. See the documentation for
+ * {@code DatabaseLocation} for information as to why this redundancy is necessary. All of these
+ * instance fields will be stored in the local cached database.
  */
-
 public class FromLocation extends DatabaseLocation {
+
     /**
-     * When stored in the Database, we will store just the id's. The object returned from the API
-     * will have the country, region, and city updated.
-     * The default value for country, region, city is 0.
+     * These instance fields mirror those in Location, but are needed for database storage
      */
     public long from_country_id;
     public long from_region_id;
     public long from_city_id;
 
+    /**
+     * Name of the country specified by the location
+     */
     public String from_country;
+
+    /**
+     * Name of the region specified by the location
+     */
     public String from_region;
+
+    /**
+     * Name of the city specified by the location
+     */
     public String from_city;
 
+    /**
+     * Population of the location
+     */
     public long from_population;
 
+    // TODO: Handle undefined geographical areas (e.g. no region defined)
+    /**
+     * Initialize instance fields with provided parameters
+     * @param cityId ID of city
+     * @param regionId ID of region
+     * @param countryId ID of country
+     * @param cityName Name of city
+     * @param regionName Name of region
+     * @param countryName Name of country
+     * @param population Population of location
+     */
     public FromLocation(long cityId, long regionId,long countryId, String cityName,
                         String regionName, String countryName, long population) {
         super(cityId, regionId, countryId);
         initialize(cityName, regionName, countryName, population);
     }
 
+    /**
+     * Initialize this class's instance fields based on those provided and those from superclass
+     * methods. This is what keeps the instance fields in sync with those of Location.
+     * @param cityName Name of city
+     * @param regionName Name of region
+     * @param countryName Name of country
+     * @param population Population of location
+     */
     private void initialize(String cityName, String regionName, String countryName, long population) {
         from_country_id = getCountryId();
         from_region_id = getRegionId();
@@ -43,18 +71,34 @@ public class FromLocation extends DatabaseLocation {
         from_population = population;
     }
 
+    /**
+     * Get the name of the city
+     * @return City name
+     */
     public String getCityName() {
         return from_city;
     }
 
+    /**
+     * Get the name of the region
+     * @return Region name
+     */
     public String getRegionName() {
         return from_region;
     }
 
+    /**
+     * Get the name of the country
+     * @return Country name
+     */
     public String getCountryName() {
         return from_country;
     }
 
+    /**
+     * Get the population of the location
+     * @return Location population
+     */
     public long getNumUsers() {
         return from_population;
     }
--- app/src/main/java/org/codethechange/culturemesh/models/NearLocation.java
+++ app/src/main/java/org/codethechange/culturemesh/models/NearLocation.java
@@ -1,32 +1,64 @@
 package org.codethechange.culturemesh.models;
 
 /**
- * Created by Drew Gregory on 2/19/18.
- * Exact copy of Location, but used as Embedded Entity in SQLite Database.
+ * Wrapper for {@code DatabaseLocation} that is for Near locations. See the documentation for
+ * {@code DatabaseLocation} for information as to why this redundancy is necessary. All of these
+ * instance fields will be stored in the local cached database.
  */
-
 public class NearLocation extends DatabaseLocation {
+
     /**
-     * When stored in the Database, we will store just the id's. The object returned near the API
-     * will have the country, region, and city updated.
-     * The default value for country, region, city is 0.
+     * These instance fields mirror those in Location, but are needed for database storage
      */
     public long near_country_id;
     public long near_region_id;
     public long near_city_id;
 
+    /**
+     * Name of the country specified by the location
+     */
     public String near_country;
+
+    /**
+     * Name of the region specified by the location
+     */
     public String near_region;
+
+    /**
+     * Name of the city specified by the location
+     */
     public String near_city;
 
+    /**
+     * Population of the location
+     */
     public long near_population;
 
+    // TODO: Handle undefined geographical areas (e.g. no region defined)
+    /**
+     * Initialize instance fields with provided parameters
+     * @param cityId ID of city
+     * @param regionId ID of region
+     * @param countryId ID of country
+     * @param cityName Name of city
+     * @param regionName Name of region
+     * @param countryName Name of country
+     * @param population Population of location
+     */
     public NearLocation(long cityId, long regionId,long countryId, String cityName,
                         String regionName, String countryName, long population) {
         super(cityId, regionId, countryId);
         initialize(cityName, regionName, countryName, population);
     }
 
+    /**
+     * Initialize this class's instance fields based on those provided and those from superclass
+     * methods. This is what keeps the instance fields in sync with those of Location.
+     * @param cityName Name of city
+     * @param regionName Name of region
+     * @param countryName Name of country
+     * @param population Population of location
+     */
     private void initialize(String cityName, String regionName, String countryName, long population) {
         near_country_id = getCountryId();
         near_region_id = getRegionId();
@@ -39,18 +71,34 @@ public class NearLocation extends DatabaseLocation {
         near_population = population;
     }
 
+    /**
+     * Get the name of the city
+     * @return City name
+     */
     public String getCityName() {
         return near_city;
     }
 
+    /**
+     * Get the name of the region
+     * @return Region name
+     */
     public String getRegionName() {
         return near_region;
     }
 
+    /**
+     * Get the name of the country
+     * @return Country name
+     */
     public String getCountryName() {
         return near_country;
     }
 
+    /**
+     * Get the population of the location
+     * @return Location population
+     */
     public long getNumUsers() {
         return near_population;
     }
