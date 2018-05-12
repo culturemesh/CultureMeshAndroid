031813db09aa3f02a51e29b7921bcfaf70f87f67
cs <cs.temporary@icloud.com>
2018-05-12 18:47:26 +0000

    Document DatabaseLocation.java

--- app/src/main/java/org/codethechange/culturemesh/models/DatabaseLocation.java
+++ app/src/main/java/org/codethechange/culturemesh/models/DatabaseLocation.java
@@ -6,21 +6,63 @@ import org.codethechange.culturemesh.Listable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Superclass for Locations that will be stored in the database. Since the instance field names
+ * are used directly as column names in the database, a single class cannot be used for both
+ * From and Near locations (the column names would conflict). Therefore, two separate classes,
+ * {@code FromLocation} and {@code NearLocation} are used. They are nearly identical, however,
+ * so this superclass holds methods common to both. It also imposes requirements on them to ensure
+ * that those methods can function. The database will store the IDs and names of the city, region,
+ * and country. It will also store the population of the described location.
+ */
 public abstract class DatabaseLocation extends Location implements Listable {
+
+    /**
+     * Constructor that passes all parameters to superclass constructor
+     * @param countryId ID of country
+     * @param regionId ID of region
+     * @param cityId ID of city
+     */
     public DatabaseLocation(long countryId, long regionId, long cityId) {
         super(countryId, regionId, cityId);
     }
+
+    /**
+     * Constructor that passes all parameters to superclass constructor
+     * @param json JSON object that defines the location. See superclass constructor documentation.
+     * @throws JSONException May be thrown for improperly formatted JSON
+     */
     public DatabaseLocation(JSONObject json) throws JSONException {
         super(json);
     }
 
+    /**
+     * Requires that subclasses store the name of the country. If the country is unspecified,
+     * the name should be set to the constant {@code Place.NOWHERE}
+     * @return Country name
+     */
     @NonNull
     public abstract String getCountryName();
+    /**
+     * Requires that subclasses store the name of the region. If the region is unspecified,
+     * the name should be set to the constant {@code Place.NOWHERE}
+     * @return Region name
+     */
     @NonNull
     public abstract String getRegionName();
+    /**
+     * Requires that subclasses store the name of the city. If the city is unspecified,
+     * the name should be set to the constant {@code Place.NOWHERE}
+     * @return City name
+     */
     @NonNull
     public abstract String getCityName();
 
+    /**
+     * Form a single name for the specified location using the format
+     * {@code [city], [region], [country]}, with unspecified geographical areas excluded
+     * @return Name of the location
+     */
     public String toString() {
         String string = "";
         string += getCountryName();
@@ -35,6 +77,11 @@ public abstract class DatabaseLocation extends Location implements Listable {
         return string;
     }
 
+    /**
+     * Get the most specific name of the geographical areas defined by the Location. For example,
+     * if the region and country are defined, the name of the region would be returned.
+     * @return Most specific of the location's names
+     */
     public String shortName() {
         if (! getCityName().equals(Place.NOWHERE)) {
             return getCityName();
@@ -45,6 +92,10 @@ public abstract class DatabaseLocation extends Location implements Listable {
         }
     }
 
+    /**
+     * Get the name that should be displayed in UI lists (also used outside lists)
+     * @return Name for UI
+     */
     public String getListableName() {
         return toString();
     }
