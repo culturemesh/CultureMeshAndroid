d7b6d3f3f0bfc68f2531846eca1d25034e5b97be
cs <cs.temporary@icloud.com>
2018-05-19 00:55:11 +0000

    Remove Network and replace with NetworkLite renamed to Network

--- app/src/main/java/org/codethechange/culturemesh/models/Network.java
+++ app/src/main/java/org/codethechange/culturemesh/models/Network.java
@@ -1,30 +1,43 @@
 package org.codethechange.culturemesh.models;
 
+import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.io.Serializable;
 
-/**
- * Created by nathaniel on 11/10/17.
- */
+@Entity
+public class Network implements Serializable {
+    @PrimaryKey
+    public long id;
 
-public class Network extends NetworkLite implements Serializable {
-    //TODO: Figure out Room deals with bigints.
+    @Embedded
+    public Location nearLocation;
+
+    @Embedded
+    public Location fromLocation;
+
+    @Embedded
+    public Language language;
 
     /**
-     * We don't have posts, events, and users fields because they are so large and often unnecessary.
-     * Instead, pass the id into an API.Get.networkUsers(), API.Get.networkEvents(), or
-     * or API.networkPosts() (asynchronously of course) to get these fields.
-     * */
+     * networkClass is a boolean determining if a network is fromloc->nearLoc or language->nearLoc.
+     * true: fromLoc->nearLoc
+     * false: lang->nearLoc
+     */
+    public boolean networkClass;
 
-    public Network(Place nearLocation, Place fromLocation, long id) {
-        super(nearLocation, fromLocation, id);
+    public Network(Location nearLocation, Location fromLocation, long id) {
+        this.fromLocation = fromLocation;
+        this.nearLocation = nearLocation;
+        networkClass = true;
+        this.id = id;
     }
 
-    public Network(Place nearLocation, Language lang, long id) {
-        super(nearLocation, lang, id);
+    public Network(Location nearLocation, Language lang, long id) {
+        this.language = lang;
+        this.nearLocation = nearLocation;
+        networkClass = false;
+        this.id = id;
     }
-
-    //No override of toString() because getResources() requires an activity
-    //We want to use string xml resources for localization.
-    //For more info, check out
-    //https://developer.android.com/guide/topics/resources/providing-resources.html
 }
