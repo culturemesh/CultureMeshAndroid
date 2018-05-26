b3f4815dc19e8df3412005f037a000dd0ca54995
cs <cs.temporary@icloud.com>
2018-05-26 05:09:00 +0000

    Document Listable.java

--- app/src/main/java/org/codethechange/culturemesh/Listable.java
+++ app/src/main/java/org/codethechange/culturemesh/Listable.java
@@ -1,8 +1,19 @@
 package org.codethechange.culturemesh;
 
-import android.view.View;
-
+/**
+ * Interface for objects that need to be listed in the user interface.
+ */
 public interface Listable {
+
+    /**
+     * Get a label to display as an identifier for the object
+     * @return Displayable name for the object
+     */
     String getListableName();
+
+    /**
+     * Get the number of users, which will be displayed under the person icon in lists
+     * @return Number of users associated with the object
+     */
     long getNumUsers();
 }
