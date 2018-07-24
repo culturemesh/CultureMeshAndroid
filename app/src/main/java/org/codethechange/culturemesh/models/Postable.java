6b31f44011a4df786fe7fd72f19f151d934cf1b8
cs <cs.temporary@icloud.com>
2018-07-24 23:40:56 +0000

    Document Postable.java and Putable.java

--- app/src/main/java/org/codethechange/culturemesh/models/Postable.java
+++ app/src/main/java/org/codethechange/culturemesh/models/Postable.java
@@ -1,8 +1,25 @@
 package org.codethechange.culturemesh.models;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Classes that implement this interface can be sent in the bodies of requests sent using
+ * {@link org.codethechange.culturemesh.API.Post#model(RequestQueue, Postable, String,
+ * String, Response.Listener)}.
+ */
 public interface Postable {
-    public abstract JSONObject getPostJson() throws JSONException;
+
+    /**
+     * Generates a JSON representation of the object that can be used in POST requests to the server.
+     * The exact format of the JSON depends upon the specifications of the server API. See the
+     * server's Swagger documentation for more.
+     * @return JSON representation of the object suitable for inclusion in the bodies of POST requests
+     * @throws JSONException May be thrown if any of the values to include in the JSON are
+     * incompatible with the JSON format
+     */
+    JSONObject getPostJson() throws JSONException;
 }
