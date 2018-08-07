c03808111d95590f3a91b7b1e543c79e2e1a9a53
cs <cs.temporary@icloud.com>
2018-08-07 01:27:55 +0000

    Document ListenableEditText

--- app/src/main/java/org/codethechange/culturemesh/ListenableEditText.java
+++ app/src/main/java/org/codethechange/culturemesh/ListenableEditText.java
@@ -1,39 +1,53 @@
 package org.codethechange.culturemesh;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
 
 /**
  * This is a custom EditText that allows us to listen for changes in cursor position.
- * CreatePostActivity uses this view so that the format toggle buttons can update their settings
- * when a new near_region in the edit text is selected.
+ * {@link CreatePostActivity} uses this view so that the format toggle buttons can update their
+ * settings when a new near_region in the edit text is selected.
  */
 public class ListenableEditText extends EditText {
 
     onSelectionChangedListener mListener = null;
 
-    int prevSelStart = 0;
+    /**
+     * {@inheritDoc}
+     * @param context {@inheritDoc}
+     */
     public ListenableEditText(Context context) {
         super(context);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param context {@inheritDoc}
+     * @param attrs {@inheritDoc}
+     */
     public ListenableEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param context {@inheritDoc}
+     * @param attrs {@inheritDoc}
+     * @param defStyleAttr {@inheritDoc}
+     */
     public ListenableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * When the selection changes, if it is due to the user typing a character,
+     * {@link ListenableEditText#mListener#onSelectionChanged(int, int)} is called with the
+     * provided parameters. Otherwise, the superclass method
+     * {@link EditText#onSelectionChanged(int, int)} is called with the parameters.
+     * @param selStart TODO: What is this?
+     * @param selEnd TODO: What is this?
+     */
     @Override
     protected void onSelectionChanged(int selStart, int selEnd) {
         if (selStart - 1 != selStart && mListener != null) {
@@ -43,12 +57,18 @@ public class ListenableEditText extends EditText {
         super.onSelectionChanged(selStart, selEnd);
     }
 
+    /**
+     * Interface that all listeners for {@link ListenableEditText#mListener} must satisfy.
+     */
     public interface onSelectionChangedListener {
         void onSelectionChanged(int selStart, int selEnd);
     }
 
+    /**
+     * Set the listener to the provided parameter
+     * @param listener Listener to use when text selection changes
+     */
     public void setOnSelectionChangedListener(onSelectionChangedListener listener) {
         mListener = listener;
     }
-
 }
