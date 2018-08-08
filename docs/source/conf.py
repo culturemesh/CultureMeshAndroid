4aa01f41c794b997b77bda059c24d6472d8bf387
cs <cs.temporary@icloud.com>
2018-08-08 19:43:29 +0000

    Increment version number to 0.2.0-beta

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, CultureMesh'
 author = 'Stanford CodeTheChange'
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0-alpha'
+release = '0.2.0-beta'
 
 
 # -- General configuration ---------------------------------------------------
