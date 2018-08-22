e49c3771df5d4758d75dd023785313e4224e8466
DrewGregory <djgregny@gmail.com>
2018-08-21 23:11:02 -0700

    Explore working!!

--- app/src/main/java/org/codethechange/culturemesh/ExploreBubblesOpenGLActivity.java
+++ app/src/main/java/org/codethechange/culturemesh/ExploreBubblesOpenGLActivity.java
@@ -3,6 +3,7 @@ package org.codethechange.culturemesh;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -12,7 +13,6 @@ import android.widget.TextView;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.squareup.picasso.Picasso;
 
 import org.codethechange.bubblepicker.BubblePickerListener;
 import org.codethechange.bubblepicker.adapter.BubblePickerAdapter;
@@ -20,14 +20,18 @@ import org.codethechange.bubblepicker.model.BubbleGradient;
 import org.codethechange.bubblepicker.model.PickerItem;
 import org.codethechange.bubblepicker.model.PickerItemSize;
 import org.codethechange.bubblepicker.rendering.BubblePicker;
+import org.codethechange.culturemesh.models.FromLocation;
+import org.codethechange.culturemesh.models.Language;
+import org.codethechange.culturemesh.models.Location;
+import org.codethechange.culturemesh.models.NearLocation;
 import org.codethechange.culturemesh.models.Network;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Display moving bubbles which show suggested networks for the user to join
- * TODO: Document this more fully
  */
 public class ExploreBubblesOpenGLActivity extends DrawerActivity {
     /**
@@ -50,7 +54,17 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
      */
     TextView hintText;
 
-    ArrayList<String> titles = new ArrayList<>();
+    /**
+     * A mapping from the title of the bubble (Location#getShortName()) to the location object.
+     */
+    HashMap<String, Location> locations;
+
+    /**
+     * A mapping from the title of the bubble (Location#getShortName()) to the language object.
+     */
+    HashMap<String, Language> languages;
+
+    NearLocation selectedNearLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +72,14 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
         setContentView(R.layout.activity_explore_bubbles_open_gl);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        title = findViewById(R.id.titleTextView);
+        subTitle = findViewById(R.id.subtitleTextView);
+        hintText = findViewById(R.id.hintTextView);
         RequestQueue queue = Volley.newRequestQueue(ExploreBubblesOpenGLActivity.this);
         picker = findViewById(R.id.picker);
         picker.setVisibility(View.GONE);
-        titles.add("something");
-        titles.add("anotha");
+        locations = new HashMap<>();
+        languages = new HashMap<>();
         setAdapter();
         API.Get.topTen(queue, new Response.Listener<NetworkResponse<ArrayList<Network>>>() {
             @Override
@@ -83,13 +100,15 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
 
     private void showBubbles(ArrayList<Network> payload) {
         for (Network net: payload) {
-            Log.i("showBubbles", "adding " + net.fromLocation.getListableName() + " " + net.nearLocation.getListableName());
+            Log.i("showBubbles", "adding " + net.fromLocation.getShortName() + " " + net.nearLocation.getShortName());
             if (net.isLocationBased()) {
-                titles.add(net.fromLocation.getListableName());
+                locations.put(net.fromLocation.getShortName(), net.fromLocation);
+            } else {
+                languages.put(net.language.name, net.language);
             }
-            titles.add(net.nearLocation.getListableName());
+            locations.put(net.nearLocation.getShortName(), net.nearLocation);
         }
-        // This our way of refres
+        // This is our way of refreshing.
         setAdapter();
         picker.onPause();
         picker.onResume();
@@ -100,6 +119,10 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
     private void setAdapter() {
         final TypedArray colors = getResources().obtainTypedArray(R.array.colors);
         final TypedArray images = getResources().obtainTypedArray(R.array.images);
+        //Convert key sets into list of strings.
+        final ArrayList<String> titles = new ArrayList<>();
+        titles.addAll(locations.keySet());
+        titles.addAll(languages.keySet());
         picker.setAdapter(new BubblePickerAdapter() {
             @Override
             public int getTotalCount() {
@@ -132,13 +155,55 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
         images.recycle();
 
         picker.setBubbleSize(70);
-        picker.setBackground(0x8BC34A);
+        picker.setBackground(0xFFFFFF);
         picker.setListener(new BubblePickerListener() {
             @Override
             public void onBubbleSelected(@NotNull PickerItem item) {
-                thisActivity.getSharedPreferences(API.SETTINGS_IDENTIFIER, MODE_PRIVATE).edit()
-                        .putLong(API.SELECTED_NETWORK, 1).apply();
-                startActivity(new Intent(thisActivity, TimelineActivity.class));
+                if (selectedNearLocation == null) {
+                    // First thing they select is their near location.
+                    if (locations.containsKey(item.getTitle())) {
+                        selectedNearLocation = locations.get(item.getTitle()).getNearLocation();
+                        title.setText(item.getTitle());
+                        subTitle.setText(getResources().getString(R.string.from_location_clarify));
+                    } else {
+                       // They selected a language instead, which is invalid as a near location.
+                       // Let's notify them of their error via a snack bar.
+                       item.setSelected(false);
+                       Snackbar.make(picker, getResources().getString(R.string.cannot_write_empty),
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    // They have selected their second component! Let's create this network.
+                    long selectedNetwork = -1;
+                    if (locations.containsKey(item.getTitle())) {
+                        // From location.
+                        FromLocation fromLoc = locations.get(item.getTitle()).getFromLocation();
+                        API.Get.netFromFromAndNear(queue, fromLoc, selectedNearLocation, new Response.Listener<NetworkResponse<Network>>() {
+                            @Override
+                            public void onResponse(NetworkResponse<Network> response) {
+                                if (response.fail()) {
+                                    response.showErrorDialog(ExploreBubblesOpenGLActivity.this);
+                                } else {
+                                    visitNetwork(response.getPayload().id);
+                                }
+                            }
+                        });
+                    } else {
+                        // Language
+                        Language language = languages.get(item.getTitle());
+                        API.Get.netFromLangAndNear(queue, language, selectedNearLocation, new Response.Listener<NetworkResponse<Network>>() {
+                            @Override
+                            public void onResponse(NetworkResponse<Network> response) {
+                                if (response.fail()) {
+                                    response.showErrorDialog(ExploreBubblesOpenGLActivity.this);
+                                } else {
+                                    visitNetwork(response.getPayload().id);
+                                }
+                            }
+                        });
+                    }
+
+                }
             }
 
             @Override
@@ -146,6 +211,17 @@ public class ExploreBubblesOpenGLActivity extends DrawerActivity {
 
             }
         });
+    }
+
+
+    /**
+     * Navigates to TimelineActivity to view the selected network.
+     * @param id id of network.
+     */
+    void visitNetwork(long id) {
+        getSharedPreferences(API.SETTINGS_IDENTIFIER, MODE_PRIVATE).edit()
+                .putLong(API.SELECTED_NETWORK, id).apply();
+        startActivity(new Intent(ExploreBubblesOpenGLActivity.this, TimelineActivity.class));
     }
 
     @Override
