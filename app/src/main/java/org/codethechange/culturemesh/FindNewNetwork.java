a684040f10c4017162d1cf526a1e0bab969592e2
Hari Sowrirajan <4everabronco@gmail.com>
2019-02-02 14:49:19 -0800

    Created intial findnetworjk new screen

---
+++ app/src/main/java/org/codethechange/culturemesh/FindNewNetwork.java
@@ -0,0 +1,13 @@
+package org.codethechange.culturemesh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindNewNetwork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_new_network);
+    }
+}
